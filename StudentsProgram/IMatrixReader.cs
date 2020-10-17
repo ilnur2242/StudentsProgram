9abe75eb6d80fe02a53bc0b3d5c270495a79f00e
Ilnur <https://github.com/ilnur2242/StudentsProgram.git>
2020-10-17 12:23:40 +0500

    solve

---
+++ StudentsProgram/IMatrixReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StudentsProgram
+{
+    interface IMatrixReader
+    {
+        //void Read(string[] args);
+         void Read();
+    }
+}
--- StudentsProgram/Solver.cs
+++ StudentsProgram/Solver.cs
@@ -4,39 +4,39 @@ using System.IO;
 using System.Linq;
 using System.Text;
 
-
 namespace StudentsProgram
 {
-    class Program
+    class Solver : IMatrixReader, IMatrixMult, IMatrixGiver
     {
-        static void Main(string[] args)
+        private string[] s1;
+        private List<List<double>> matrix1 = new List<List<double>>();
+        private List<List<double>> matrix2 = new List<List<double>>();
+        private List<List<double>> result = new List<List<double>>();
+        private int M, N, J, K;
+        private string[] res_str;
+
+        public Solver(string[] args)
         {
-            string[] s1 = File.ReadAllLines(args[0]);
+            s1 = args;
+        }
 
-            var matr = new Solver(s1);
-            matr.Read();
-            matr.Multiplication();
-            matr.GiveMatrix();
-
-            /*//var N = Convert.ToInt32(s1[0].Split(' ')[0]);
-            //var M = Convert.ToInt32(s1[0].Split(' ')[1]);
-            int M, J; // column count for 1, 2 matr
-
+        public void Read()
+        {
             //======
             //Matrix 1
             //======
-            var N = Convert.ToInt32(s1[0].Split(' ')[0]);          
+            N = Convert.ToInt32(s1[0].Split(' ')[0]);
             try
-            {                
+            {
                 M = Convert.ToInt32(s1[0].Split(' ')[1]);
             }
-            catch(IndexOutOfRangeException e)
+            catch (IndexOutOfRangeException e)
             {
                 Console.WriteLine("enter column count for the first matrix");//
                 M = Convert.ToInt32(Console.ReadLine());
             }
 
-            var matrix1 = new List<List<double>>();
+            //var matrix1 = new List<List<double>>();
             for (int i = 1; i <= N; i++)
             {
                 matrix1.Add(new List<double>());
@@ -46,24 +46,22 @@ namespace StudentsProgram
                 }
             }
             if (matrix1.Count * matrix1[0].Count != N * M) { Console.WriteLine("Please edit input file and restart programm"); Console.ReadLine(); Environment.Exit(0); }
-            
+
             //======
             //Matrix 2
             //======
 
-            var K = Convert.ToInt32(s1[N + 1].Split(' ')[0]);
+            K = Convert.ToInt32(s1[N + 1].Split(' ')[0]);
             try
             {
-                J =  Convert.ToInt32(s1[N + 1].Split(' ')[1]);
+                J = Convert.ToInt32(s1[N + 1].Split(' ')[1]);
             }
             catch (IndexOutOfRangeException e)
             {
                 Console.WriteLine("enter column count for the second matrix");
                 J = Convert.ToInt32(Console.ReadLine());
             }
-            //var J = Convert.ToInt32(s1[N + 1].Split(' ')[1]);
-
-            var matrix2 = new List<List<double>>();
+            
             for (int i = 1; i <= K; i++)
             {
                 matrix2.Add(new List<double>());
@@ -74,13 +72,11 @@ namespace StudentsProgram
             }
             if (matrix2.Count * matrix2[0].Count != K * J) { Console.WriteLine("Please edit input file and restart programm"); Console.ReadLine(); Environment.Exit(0); }
             if (M != K) { Console.WriteLine("Count of matr1's column and count of matr2's string are not equal. Please edit input file and restart programm"); Console.ReadLine(); Environment.Exit(0); }
-            
-            //======
-            //Result matrix 
-            //======
-            
-            var result = new List<List<double>>();
-            string[] res_str = new string[N];
+        }
+   
+        public void Multiplication() 
+        {
+            res_str = new string[N];
             for (int i = 0; i < N; i++)
             {
                 result.Add(new List<double>());
@@ -89,32 +85,29 @@ namespace StudentsProgram
                     result.Last().Add(0);
                     var value = 0.0;
                     for (int _ = 0; _ < M; _++)
-                    {
-                        //var value1 = matrix1[i][_];
-                        //var value2 = matrix2[_][j];
-                        //var value = result.Last().Last();
-                        //value += value1 * value2;
-                        //result.Last()[result.Last().Count - 1] = value;
+                    {                        
                         value += matrix1[i][_] * matrix2[_][j];
-
                     }
                     result.Last()[result.Last().Count - 1] = value;
                 }
                 res_str[i] = string.Join(" ", result[i].ToArray());
             }
+        }
 
-            // Вывод результата в файл
+        public void GiveMatrix()
+        {
             string path = "Out.txt";
             if (!File.Exists(path)) File.Create(path);
 
             using (StreamWriter sw = new StreamWriter(path))
             {
                 for (int i = 0; i < result.Count; i++)
-                {                    
-                        sw.WriteLine(res_str[i]);                    
+                {
+                    sw.WriteLine(res_str[i]);
                 }
                 sw.Close();
-            }*/
+            }
         }
+
     }
 }
